de0c0ddd6c44c0b73d0f50631021dbccf9d9c652
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-12-07 20:12:01 -0300

    fix: client model

--- src/models/clients.models.js
+++ src/models/clients.models.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const clientsSchema = joi.object({
   name: joi.string().required(),
-  phone: joi.string().required(),
+  phone: joi.string().min(10).max(11).required(),
   cpf: joi.string().max(11).required(),
   birthday: joi.string().required(),
 });
