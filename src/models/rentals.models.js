8fc340c038ac8eb611a4fb9af878f71f9cdc6fae
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-12-07 21:28:29 -0300

    fix: model rentals

--- src/models/rentals.models.js
+++ src/models/rentals.models.js
@@ -3,9 +3,5 @@ import joi from "joi";
 export const rentalsSchema = joi.object({
   customerId: joi.number().required(),
   gameId: joi.number().required(),
-  rentDate: joi.string().required(),
   daysRented: joi.number().required(),
-  returnDate: joi.string(),
-  originalPrice: joi.number().required(),
-  delayFee: joi.number(),
 });
