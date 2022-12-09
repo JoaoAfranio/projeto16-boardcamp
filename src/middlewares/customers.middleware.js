88396bfa7c568f8f30f86818ae1dcfb5e441b3b7
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-12-08 21:49:41 -0300

    fix: middleware update customer

--- src/middlewares/customers.middleware.js
+++ src/middlewares/customers.middleware.js
@@ -3,13 +3,31 @@ import connection from "../database/db.js";
 
 export async function customerSchemaValidation(req, res, next) {
   const customer = req.body;
-
   const { error } = customersSchema.validate(customer);
-
   if (error) return res.sendStatus(400);
+
+  next();
+}
+
+export async function insertCustomerValidation(req, res, next) {
+  const customer = req.body;
 
   const existsCustomer = await connection.query("SELECT * FROM customers WHERE cpf= $1", [customer.cpf]);
   if (existsCustomer.rowCount !== 0) return res.sendStatus(409);
+
+  next();
+}
+
+export async function updateCustomerValidation(req, res, next) {
+  const id = Number(req.params.id);
+  const customer = req.body;
+
+  const cpfInUse = await connection.query("SELECT * FROM customers WHERE cpf= $1", [customer.cpf]);
+  const personCpf = cpfInUse.rows[0];
+
+  if (!personCpf) return next();
+
+  if (personCpf.id !== id) return res.sendStatus(409);
 
   next();
 }
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { findAllCustomers, findCustomer, insertCustomer, updateCustomer } from "../controllers/customers.controller.js";
-import { customerSchemaValidation } from "../middlewares/customers.middleware.js";
+import { customerSchemaValidation, insertCustomerValidation, updateCustomerValidation } from "../middlewares/customers.middleware.js";
 
 const router = Router();
 
 router.get("/customers", findAllCustomers);
 router.get("/customers/:id", findCustomer);
-router.post("/customers", customerSchemaValidation, insertCustomer);
-router.put("/customers/:id", customerSchemaValidation, updateCustomer);
+router.post("/customers", customerSchemaValidation, insertCustomerValidation, insertCustomer);
+router.put("/customers/:id", customerSchemaValidation, updateCustomerValidation, updateCustomer);
 
 export default router;
